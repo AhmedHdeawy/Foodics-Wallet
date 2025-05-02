a70ae2ad6232c3e5db9e0a31a662e08bdf342bcb
AhmedHdeawy <ahmedhdeawy@gmail.com>
2025-05-02 19:21:28 +0300

    feat: create Client model and migration for clients table

---
+++ app/Models/Client.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Client extends Model
+{
+    /** @use HasFactory<UserFactory> */
+    use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'balance',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'balance' => 'decimal:2',
+        ];
+    }
+}
