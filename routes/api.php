5d2987b820b4475073362b993cdbd4e8dd30856b
AhmedHdeawy <ahmedhdeawy@gmail.com>
2025-05-04 10:01:22 +0300

    feat: install and configure Pest for testing

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,20 @@ use App\Http\Controllers\Transfer\TransferController;
 use App\Http\Controllers\Webhook\WebhookController;
 use Illuminate\Support\Facades\Route;
 
-// Webhook routes
-Route::prefix('webhooks')
-    ->name('webhooks.')
-    ->middleware('throttle:webhook')
+Route::prefix('v1')
+    ->name('api.v1.')
     ->group(function () {
-        Route::post('/{bank}', [WebhookController::class, 'handle'])->name('handle');
-        Route::get('/{id}/status', [WebhookController::class, 'status'])->name('status');
-    });
+        // Webhook routes
+        Route::prefix('webhooks')
+            ->name('webhooks.')
+            ->middleware('throttle:webhook')
+            ->group(function () {
+                Route::post('/{bank}', [WebhookController::class, 'handle'])->name('handle');
+                Route::get('/{id}/status', [WebhookController::class, 'status'])->name('status');
+            });
 
-Route::post('transfer', [TransferController::class, 'transfer'])
-    ->name('transfer.handle')
-    ->middleware('throttle:transfer');
+        // Send money routes
+        Route::post('transfer', [TransferController::class, 'transfer'])
+            ->name('transfer.handle')
+            ->middleware('throttle:transfer');
+    });
